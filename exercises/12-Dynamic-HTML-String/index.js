3a2fa15bb0edd7b4d7f8be78bd5870205ccb68f1
ada_schuler <ada.schuler@bicevida.cl>
2022-02-04 14:08:13 -0300

    DOM 13/14

--- exercises/12-Dynamic-HTML-String/index.js
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -1,2 +1,10 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong>";
 document.write(myString);
+     
+let myString2 = document.createElement("span");
+    myString2.innerHTML +=", we are in the year ";
+    document.body.appendChild(myString2);    
+let fecha = new Date();
+let anual = fecha.getFullYear();
+console.log(fecha.getFullYear());
+myString2.append(anual);
--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,2 +1,17 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+//selecciono el select
+const selectionSelect=document.querySelector("#mySelect");
+
+//recorro el array
+for (let i = 0; i < countries.length; i++) {
+		let option = document.createElement("option"); 
+		option.text = countries[i];
+		selectionSelect.add(option);
+		document.querySelector("#mySelect").appendChild(option); 
+	}
+//añado el evento    
+    selectionSelect.addEventListener("change", function() {
+		alert(event.target.value);
+	});
